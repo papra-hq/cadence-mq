fce2b9806576b382b5bd29260edbf602deda20a8
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-09-13 23:45:12 +0200

    chore(package): added provenance

---
+++ .changeset/neat-clubs-fall.md
@@ -0,0 +1,9 @@
+---
+"@cadence-mq/driver-drizzle": patch
+"@cadence-mq/driver-kysely": patch
+"@cadence-mq/driver-libsql": patch
+"@cadence-mq/driver-memory": patch
+"@cadence-mq/core": patch
+---
+
+Removed sourcemaps from published package
--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: ['./src/index.ts', './src/utils'],
-  dts: { sourcemap: true },
-  sourcemap: true,
   clean: true,
   exports: true, // auto update the package.json exports, main, module and types fields
 });
